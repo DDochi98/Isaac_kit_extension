8a31fe62c23b53b23135ce27db11b0caa3a22cb7
DDoch98 <wnghks6884@naver.com>
2023-08-14 16:20:39 +0900

    Using the WheeledRobot Class

--- user_examples/hello_world.py
+++ user_examples/hello_world.py
@@ -10,7 +10,8 @@ from omni.isaac.examples.base_sample import BaseSample
 from omni.isaac.core.utils.types import ArticulationAction
 from omni.isaac.core.utils.nucleus import get_assets_root_path
 from omni.isaac.core.utils.stage import add_reference_to_stage
-from omni.isaac.core.robots import Robot
+from omni.isaac.wheeled_robots.robots import WheeledRobot
+#from omni.isaac.core.robots import Robot
 
 import numpy as np
 
@@ -24,23 +25,34 @@ class HelloWorld(BaseSample):
         world.scene.add_default_ground_plane()
         # you configure a new server with /Isaac folder in it
         assets_root_path = get_assets_root_path()
-        if assets_root_path is None:
-            # Use carb to log warnings, errors and infos in your application (shown on terminal)
-            carb.log_error("Could not find nucleus server with /Isaac folder")
-        asset_path = assets_root_path + "/Isaac/Robots/Jetbot/jetbot.usd"
+        #if assets_root_path is None:
+        #    # Use carb to log warnings, errors and infos in your application (shown on terminal)
+        #    carb.log_error("Could not find nucleus server with /Isaac folder")
+        #asset_path = assets_root_path + "/Isaac/Robots/Jetbot/jetbot.usd"
+        jetbot_asset_path = assets_root_path + "/Isaac/Robots/Jetbot/jetbot.usd"
+
         # This will create a new XFormPrim and point it to the usd file as a reference
         # Similar to how pointers work in memory
-        add_reference_to_stage(usd_path=asset_path, prim_path="/World/Fancy_Robot")
+        ##add_reference_to_stage(usd_path=asset_path, prim_path="/World/Fancy_Robot")
         # Wrap the jetbot prim root under a Robot class and add it to the Scene
         # to use high level api to set/ get attributes as well as initializing
         # physics handles needed..etc.
         # Note: this call doesn't create the Jetbot in the stage window, it was already
         # created with the add_reference_to_stage
-        jetbot_robot = world.scene.add(Robot(prim_path="/World/Fancy_Robot", name="fancy_robot"))
+        ##jetbot_robot = world.scene.add(Robot(prim_path="/World/Fancy_Robot", name="fancy_robot"))
         # Note: before a reset is called, we can't access information related to an Articulation
         # because physics handles are not initialized yet. setup_post_load is called after
         # the first reset so we can do so there
-        print("Num of degrees of freedom before first reset: " + str(jetbot_robot.num_dof)) # prints None
+        #print("Num of degrees of freedom before first reset: " + str(jetbot_robot.num_dof)) # prints None
+        self._jetbot = world.scene.add(
+            WheeledRobot(
+                prim_path="/World/Fancy_Robot",
+                name="fancy_robot",
+                wheel_dof_names=["left_wheel_joint", "right_wheel_joint"],
+                create_robot=True,
+                usd_path=jetbot_asset_path,
+            )
+        )
         return
 
     async def setup_post_load(self):
